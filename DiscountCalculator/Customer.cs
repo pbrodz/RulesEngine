2997391eeca73600cd6356ac369c76fc69639e28
Paul Brodzinski <pbrodz@hotmail.com>
2023-03-28 15:28:56 -0700

    Delete DiscountCalculator directory

--- DiscountCalculator/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DiscountCalculator
-{
-    public class Customer
-    {
-        public DateTime? DateOfFirstPurchase { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-        public bool IsVeteran { get; set; }
-    }
-}
