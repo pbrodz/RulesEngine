70583ef2494d1160d6d046adc153d27e2958afcf
Paul Brodzinski <pbrodz@hotmail.com>
2023-03-28 15:33:51 -0700

    Initial Commit

---
+++ DiscountCalculator/CalculateDiscount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscountCalculator
+{
+    public class CalculateDiscount
+    {
+        public decimal CalculatePercentDiscount(Customer customer)
+        {
+            return .15m;
+        }
+    }
+}
